c7f22ffe8ec7ea8455a00aa866cd48c4c159f25d
DeepakJais <DeepakJaiswar678@gmail.com>
2018-03-07 19:45:23 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,16 @@
+num1 = int (input("Your 1st number= "))
+operator = ("What you want to do + - * / ")
+num2 = int (input("Your 2nd number= "))
+
+if operator == "+":
+    answer = num1 + num2
+elif operator == "-":
+    answer = num1 - num2
+elif operator == "*":
+    answer = num1 * num2
+elif operator == "/":
+    answer = num1 / num2
+elif operator == "**":
+    answer = num1 ** num2
+
+print("Your Answer is " + str(answer))
